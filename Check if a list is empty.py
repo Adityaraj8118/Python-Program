cbc0cfa905c8e71179e341821fc1620563b2c51d
Aditya Raj Gupta <161880450+Adityaraj8118@users.noreply.github.com>
2024-10-16 22:40:47 +0530

    Update and rename Check if a list is empty to Check if a list is empty.py

--- Check if a list is empty.py
+++ Check if a list is empty.py
@@ -3,6 +3,6 @@ def check_if_empty(lst):
         print("The list is empty.")
     else:
         print(f"The list is not empty. It contains {len(lst)} elements.")
-
+        
 my_list = []
 check_if_empty(my_list)
