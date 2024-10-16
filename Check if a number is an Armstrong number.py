575fe3d1b952863e99be8b65c8886d0f570fef89
Aditya Raj Gupta <161880450+Adityaraj8118@users.noreply.github.com>
2024-10-16 22:37:39 +0530

    Update and rename Check if a number is an Armstrong number to Check if a number is an Armstrong number.py

--- Check if a number is an Armstrong number.py
+++ Check if a number is an Armstrong number.py
@@ -1,4 +1,4 @@
-# An Armstrong number is a number that is equal to the sum of its digits, each raised to a power. For example, 153 is an Armstrong number because 1^3 (1) + 5^3 (125) + 3^3 (27) equals 153 
+#An Armstrong number is a number that is equal to the sum of its digits, each raised to a power. For example, 153 is an Armstrong number because 1^3 (1) + 5^3 (125) + 3^3 (27) equals 153 
 def is_armstrong(num):
     digits = [int(digit) for digit in str(num)]
     return num == sum(digit ** len(digits) for digit in digits)
