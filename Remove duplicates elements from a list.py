ed8a57cb46536fa4ba09c7cd888059ae7004cfe0
aditya raj gupta <adityaraaj62@gmail.com>
2024-10-19 22:17:52 +0530

    updated code

--- Remove duplicates elements from a list.py
+++ Remove duplicates elements from a list.py
@@ -3,4 +3,4 @@ def remove_duplicates(lst):
     print(f"List after removing duplicates: {unique_list}")
 
 my_list = [10, 20, 20, 30, 40, 40, 50]
-remove_duplicates(my_lis
+remove_duplicates(my_list)
