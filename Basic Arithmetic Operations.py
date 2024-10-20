d1b118fbf3827990678a445f27b766de97237566
aditya raj gupta <adityaraaj62@gmail.com>
2024-10-20 21:10:30 +0530

    Create new file

--- Basic Arithmetic Operations.py
+++ Basic Arithmetic Operations.py
@@ -2,13 +2,13 @@ a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
 
 sum_val = a + b
-diff_val = a - b
+Sub_val = a - b
 product_val = a * b
 quotient_val = a / b if b != 0 else 'Undefined (division by zero)'
 remainder_val = a % b
 
 print(f"Sum: {sum_val}")
-print(f"Difference: {diff_val}")
+print(f"Difference: {Sub_val}")
 print(f"Product: {product_val}")
 print(f"Quotient: {quotient_val}")
-print(f"Remainder{remainder_val}")
+print(f"Remainder: {remainder_val}")
