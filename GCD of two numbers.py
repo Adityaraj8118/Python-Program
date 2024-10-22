35a15c25541410df394309b8cb721cf16467ebb7
aditya raj gupta <adityaraaj62@gmail.com>
2024-10-22 23:31:52 +0530

    Create new file

---
+++ GCD of two numbers.py
@@ -0,0 +1,7 @@
+import math
+def find_gcd(a, b):
+    return math.gcd(a, b)
+num1 = 48
+num2 = 60
+gcd = find_gcd(num1, num2)
+print(f"GCD of {num1} and {num2} is {gcd}")
---
+++ Pythagorean Theorem.py
@@ -0,0 +1,7 @@
+import math
+
+a = float(input("Enter side a: "))
+b = float(input("Enter side b: "))
+# Calculate the hypotenuse
+hypotenuse = math.sqrt(a**2 + b**2)
+print(f"The hypotenuse is {hypotenuse}")
